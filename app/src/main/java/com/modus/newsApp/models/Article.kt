a11782ea59bba928cad086b24b9eddb3faab5b53
Doha <DohaEhab@gmail.com>
2021-05-23 01:10:15 +0200

    List breaking news

--- app/src/main/java/com/modus/newsApp/models/Article.kt
+++ app/src/main/java/com/modus/newsApp/models/Article.kt
@@ -3,10 +3,10 @@ package com.modus.newsApp.models
 data class Article(
     val author: String?,
     val content: String?,
-    val description: String,
+    val description: String?,
     val publishedAt: String,
     val source: Source,
-    val title: String,
-    val url: String,
-    val urlToImage: String
+    val title: String?,
+    val url: String?,
+    val urlToImage: String?
 )
